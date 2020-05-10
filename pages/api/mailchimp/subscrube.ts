bf81f6911f49dbafd1671d618f9c6acc3081ef87
awitherow <au.witherow@gmail.com>
2020-05-10 13:11:24 -1000

    add mailchimp subscription api endpoint

--- pages/api/mailchimp/subscrube.ts
+++ pages/api/mailchimp/subscrube.ts
@@ -1,7 +1,9 @@
-const Mailchimp = require("mailchimp-api-v3");
+import Mailchimp from "mailchimp-api-v3";
+import { NowRequest, NowResponse } from "@now/node";
+
 const mailchimp = new Mailchimp(process.env.mailchimp);
 
-module.exports = async (req, res) => {
+module.exports = async (req: NowRequest, res: NowResponse) => {
   const { email_address, first_name, source } = req.body;
 
   try {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import axios from "axios";
-import Head from "next/head";
 
 import styles from "./index.module.scss";
 
@@ -43,36 +42,40 @@ export default function Home() {
   return (
     <Layout>
       <div className={styles.cover}>
-        <div className={styles.signup}>
-          <h3>Essential Oils On and Off the Mat</h3>
-          <p>
-            Sign up for the free Masterclass and learn how to empower your
-            personal practice with Essential Oils.
-          </p>
-          <p>BONUS: Get the Essential Oil Sadhana Cheatsheet FREE</p>
-          <Form.Control
-            type="text"
-            placeholder="First Name"
-            autoComplete="given-name"
-            value={first_name}
-            onChange={(e) => updateName(e.target.value)}
-          />
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            autoComplete="email"
-            value={email_address}
-            onChange={(e) => updateEmail(e.target.value)}
-          />
+        <div className="container">
+          <div className={styles.signup}>
+            <h3>Essential Oils On and Off the Mat</h3>
+            <p>
+              Sign up for the free Masterclass: Learn how to Empower your
+              Practice and Self Care with Essential Oils.
+            </p>
+            <p>BONUS: Get the Essential Oil Sadhana Printout for FREE!</p>
+            <Form>
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                autoComplete="given-name"
+                value={first_name}
+                onChange={(e) => updateName(e.target.value)}
+              />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                autoComplete="email"
+                value={email_address}
+                onChange={(e) => updateEmail(e.target.value)}
+              />
 
-          <div className="btn-container">
-            <Button
-              disabled={success === "good"}
-              onClick={() => handleSignup()}
-              variant={successMap[success].variant}
-            >
-              {successMap[success].text}
-            </Button>
+              <div className="btn-container">
+                <Button
+                  disabled={success === "good"}
+                  onClick={() => handleSignup()}
+                  variant={successMap[success].variant}
+                >
+                  {successMap[success].text}
+                </Button>
+              </div>
+            </Form>
           </div>
         </div>
       </div>
